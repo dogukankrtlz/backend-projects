e3511940f35ff13e3fc819eba91c7a2fd37d3362
Doğukan Kertlez <dogukankertlez562@hotmail.com>
2022-06-24 14:42:11 +0300

    editing file paths

---
+++ grpc/spring-boot-grpc-kafka/src/main/java/com/bkteam/greeter/kafka/KafkaConsumer.java
@@ -0,0 +1,28 @@
+package com.bkteam.greeter.kafka;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+
+    @KafkaListener(topics = "output")
+    public void receiveMessage(String message) {
+        LOGGER.info("received message='{}'", message);
+        System.out.println("received message=" + message);
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public void KafkaConsumer(){}
+}
